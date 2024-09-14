20df49c87c68642587422657effa9ddb86ed7116
brettfloyd <brettfloyd@zeroeyes.com>
2024-09-14 13:20:02 -0400

    Changed the start method to run, as start was depricated per docs

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "github.com/brfloyd/senior-project-brett-cli-data-project/tui"  
-    tea "github.com/charmbracelet/bubbletea"
+	"fmt"
+	"github.com/brfloyd/senior-project-brett-cli-data-project/tui"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
 )
 
 func main() {
-    
-    model := tui.InitialModel()  
-    p := tea.NewProgram(model)  
-    if err := p.Start(); err != nil {
-        fmt.Println("Error running program:", err)
-        os.Exit(1)
-    }
-}
 
+	model := tui.InitialModel()
+	p := tea.NewProgram(model)
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
